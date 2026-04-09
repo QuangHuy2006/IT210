bd3fe49db74c23a440ccd0b5a2acb36598580a5d
QuangHuy2006 <tonphamquanghuy3@Gmail.com>
2026-04-10 01:01:30 +0700

    Add files via upload

---
+++ ss4/src/main/java/org/example/session4/Bai1/repository/OrderRepository.java
@@ -0,0 +1,15 @@
+package org.example.ss4.src.main.java.org.example.session4.Bai1.repository;
+
+
+import org.springframework.stereotype.Repository;
+
+@Repository("OrderRepositoryB1")
+public class OrderRepository {
+    public String getAllOrders() {
+        return "Danh sach toan bo don hang";
+    }
+
+    public String getOrderById(Long id) {
+        return "Thong tin don hang voi ID: " + id;
+    }
+}
